2404ded0e3fbfc78bc855b3d5addebe329c45319
M.Hayden <m.hayden@vikingelectronics.com>
2021-02-05 09:57:26 -0600

    initial setup of new activity and application

---
+++ Softphone/app/src/main/java/com/vikingelectronics/softphone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vikingelectronics.softphone
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+}
---
+++ Softphone/app/src/main/java/com/vikingelectronics/softphone/SoftphoneApplication.kt
@@ -0,0 +1,6 @@
+package com.vikingelectronics.softphone
+
+import android.app.Application
+
+class SoftphoneApplication: Application() {
+}
