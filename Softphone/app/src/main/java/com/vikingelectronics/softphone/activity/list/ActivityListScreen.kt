e7fbe49e905ea9950683a53b561ccc36478421c0
M.Hayden <m.hayden@vikingelectronics.com>
2021-04-06 13:25:40 -0500

    AND-14 - ActivityList now fully composable

--- Softphone/app/src/main/java/com/vikingelectronics/softphone/activity/list/ActivityListScreen.kt
+++ Softphone/app/src/main/java/com/vikingelectronics/softphone/activity/list/ActivityListScreen.kt
@@ -1,9 +1,5 @@
 package com.vikingelectronics.softphone.activity.list
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -12,40 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import com.vikingelectronics.softphone.R
 import com.vikingelectronics.softphone.activity.ActivityEntry
 import com.vikingelectronics.softphone.activity.ActivityEntryCard
 import com.vikingelectronics.softphone.extensions.setParcelableAndNavigate
 import com.vikingelectronics.softphone.navigation.Screen
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class ActivityListFragment: Fragment(R.layout.fragment_generic_compose) {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)?.apply {
-            findViewById<ComposeView>(R.id.composeView).setContent {
-                MaterialTheme {
-                    ActivityListScreen(findNavController())
-                }
-            }
-        }
-    }
-}
-
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
