dc28cdd7230b3b1d1683ec6bbb311ba06c6f9da6
M.Hayden <m.hayden@vikingelectronics.com>
2021-04-06 13:15:32 -0500

    AND-12 - removed DeviceListFragment.kt in favor of DeviceListScreen.kt, device list now fully in compose

--- Softphone/app/src/main/java/com/vikingelectronics/softphone/devices/list/DeviceListScreen.kt
+++ Softphone/app/src/main/java/com/vikingelectronics/softphone/devices/list/DeviceListScreen.kt
@@ -1,46 +1,17 @@
 package com.vikingelectronics.softphone.devices.list
 
-import android.os.Bundle
-import android.os.Parcelable
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
-import androidx.navigation.findNavController
-import com.vikingelectronics.softphone.R
 import com.vikingelectronics.softphone.devices.DeviceCard
 import com.vikingelectronics.softphone.extensions.setParcelableAndNavigate
 import com.vikingelectronics.softphone.navigation.Screen
-import dagger.hilt.android.AndroidEntryPoint
-
-
-@AndroidEntryPoint
-class DeviceListFragment: Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_generic_compose, container, false).apply {
-            findViewById<ComposeView>(R.id.composeView).setContent {
-                MaterialTheme {
-//                    DevicesListScreen(findNavController())
-                }
-            }
-        }
-    }
-}
-
 
 @Composable
 fun DevicesListScreen(
